6b9c9a7bc1b0716d3fbee968ffac783b86578cb3
rongyuan <zhaorongyuan87@163.com>
2026-06-12 17:28:25 +0800

    添加WIFI初始化功能并更新主函数中的USART打印初始化

--- User/main.c
+++ User/main.c
@@ -50,6 +50,41 @@ void LED_Init()
 
 }
 
+/*********************************************************************
+ * @fn      WIFI_Init
+ *
+ * @brief   
+ *
+ * @return  none
+ */
+void WIFI_Init()
+{
+	GPIO_InitTypeDef GPIO_InitStructure = {0};
+	USART_InitTypeDef USART_InitStructure = {0};
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART6, ENABLE);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+	USART_InitStructure.USART_BaudRate = 115200;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Tx;
+	
+	GPIO_WriteBit(GPIOE, GPIO_Pin_11, Bit_SET);
+	GPIO_WriteBit(GPIOE, GPIO_Pin_12, Bit_SET);
+
+	USART_Init(UART6, &USART_InitStructure);
+    USART_Cmd(UART6, ENABLE);
+
+}
+
 
 /*********************************************************************
  * @fn      main
@@ -63,10 +98,10 @@ int main(void)
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
 	SystemCoreClockUpdate();
 	Delay_Init();
-	// USART_Printf_Init(115200);	
-	// printf("SystemClk:%d\r\n",SystemCoreClock);
-	// printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
-	// printf("This is printf example\r\n");
+	USART_Printf_Init(115200);	
+	printf("SystemClk:%d\r\n",SystemCoreClock);
+	printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
+	printf("This is printf example\r\n");
 
 	LED_Init();
 
