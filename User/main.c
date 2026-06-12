ee031c15fad8f5fb43cab5e09e8394b903f6fb0d
rongyuan <zhaorongyuan87@163.com>
2026-06-12 16:30:18 +0800

    new init code

---
+++ User/main.c
@@ -0,0 +1,83 @@
+/********************************** (C) COPYRIGHT *******************************
+* File Name          : main.c
+* Author             : WCH
+* Version            : V1.0.0
+* Date               : 2021/06/06
+* Description        : Main program body.
+*********************************************************************************
+* Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+* Attention: This software (modified or not) and binary are used for 
+* microcontroller manufactured by Nanjing Qinheng Microelectronics.
+*******************************************************************************/
+
+/*
+ *@Note
+ USART Print debugging routine:
+ USART1_Tx(PA9).
+ This example demonstrates using USART1(PA9) as a print debug port output.
+
+*/
+
+#include "debug.h"
+#include "ch32v30x_gpio.h"
+
+
+/* Global typedef */
+
+/* Global define */
+
+/* Global Variable */
+
+/*********************************************************************
+ * @fn      LED_Init
+ *
+ * @brief   
+ *
+ * @return  none
+ */
+void LED_Init()
+{
+	GPIO_InitTypeDef LED_GPIO_InitTypeDef = {0};
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE);
+	LED_GPIO_InitTypeDef.GPIO_Pin = GPIO_Pin_11|GPIO_Pin_12;
+	LED_GPIO_InitTypeDef.GPIO_Mode = GPIO_Mode_Out_PP;
+	LED_GPIO_InitTypeDef.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOE, &LED_GPIO_InitTypeDef);
+
+	GPIO_WriteBit(GPIOE, GPIO_Pin_11, Bit_SET);
+	GPIO_WriteBit(GPIOE, GPIO_Pin_12, Bit_SET);
+
+}
+
+
+/*********************************************************************
+ * @fn      main
+ *
+ * @brief   Main program.
+ *
+ * @return  none
+ */
+int main(void)
+{
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	SystemCoreClockUpdate();
+	Delay_Init();
+	// USART_Printf_Init(115200);	
+	// printf("SystemClk:%d\r\n",SystemCoreClock);
+	// printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
+	// printf("This is printf example\r\n");
+
+	LED_Init();
+
+
+	while(1)
+    {
+		Delay_Ms(100);
+		GPIO_WriteBit(GPIOE, GPIO_Pin_11, Bit_RESET);
+		Delay_Ms(100);
+		GPIO_WriteBit(GPIOE, GPIO_Pin_11, Bit_SET);
+
+	}
+}
+
